356bb5ef742c314025929d6c987fd2a0eeb60d32
José Luis Zúñiga <jose@tangibledesign.co>
2024-03-20 09:44:18 -0500

    Scroll snap

--- src/app/shared/componets/header/header.component.ts
+++ src/app/shared/componets/header/header.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  activeMenu = false;
+  countProduct = 0;
 
+  constructor (@Inject(DOCUMENT) private document: Document) {}
+  ngOnInit(): void{
+
+  }
+
+  toggleMenu(){
+    this.activeMenu = !this.activeMenu;
+  }
+
+  public goto(page: string) {
+    this.document?.getElementById(page)?.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest"
+    })
+  }
+
+  public gotoMobile = (page: string) => {
+    this.document?.getElementById(page)?.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest"
+    });
+    this.toggleMenu();
+  }
 }
